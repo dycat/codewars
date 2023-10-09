461591d5ddf7ce11f6989d2a7bab9a60f59e8f79
dycat <8856528+dycat@users.noreply.github.com>
2023-10-09 20:53:17 +0800

    Create sum_array.c

---
+++ sum_array.c
@@ -0,0 +1,11 @@
+
+int sum_array(const int *values, size_t count) {
+  if (count == 0) {
+    return 0;
+  }
+  int sum = 0;
+  for (int i = 0; (size_t)i < count; i++) {
+    sum += values[i];
+  }
+  return sum;
+}
